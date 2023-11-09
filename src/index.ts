721711230c9c8b574411fd02f3b1caf6405d33ed
Volodymyr Lysenko <lusiy321@gmail.com>
2023-11-09 20:10:43 +0200

    gdfgdf

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import path from "path";
 const app = express();
 
 const ipData: any = [];
-fs.createReadStream(path.resolve("ip.csv"))
+fs.createReadStream(path.join("src/ip.csv"))
   .pipe(csv())
   .on("data", (row: any) => {
     ipData.push(row);
